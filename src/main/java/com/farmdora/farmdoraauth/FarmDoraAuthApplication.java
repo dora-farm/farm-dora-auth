f6b9ffb9b49c1c1510ba8ba1cd0a0063d9d60c5a
leedahoon <ekgns0508@gmail.com>
2025-04-05 00:30:52 +0900

    Project Setting

---
+++ src/main/java/com/farmdora/farmdoraauth/FarmDoraAuthApplication.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdoraauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmDoraAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmDoraAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmdora/farmdoraauth/FarmDoraAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdoraauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmDoraAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
